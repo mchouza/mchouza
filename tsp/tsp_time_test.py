0293f98c2b2f4daa81d829e9baf7c5e446ea4815
mchouza <mchouza@746d65ee-141b-11df-a6b0-3b3dab745ba8>
2010-11-21 22:09:42 +0000

    TSP - Now with naive recursive algorithm and graph plotting code.

--- tsp/tsp_time_test.py
+++ tsp/tsp_time_test.py
@@ -3,52 +3,35 @@ import timeit
 TARGET_TIME = 0.1
 N_RANGE = (2, 10)
 
-def time_test_naive(n, rep):
+def time_test(n, rep, func_name):
     setup_stmt = """
 import random
-from tsp import tsp_naive_solve
+from tsp import %(func_name)s
 random.seed(0)
 d = [[random.randint(1, 10) if i != j else 0 for i in range(%(n)d + 1)]
      for j in range(%(n)d + 1)]
-""" % {'n': n}
-    naive_timed_stmt = 'tsp_naive_solve(d)'
-    naive_time = timeit.timeit(naive_timed_stmt, setup_stmt, number=rep) / rep
-    return naive_time
+""" % {'n': n, 'func_name': func_name}
+    timed_stmt = '%s(d)' % func_name
+    time = timeit.timeit(timed_stmt, setup_stmt, number=rep) / rep
+    return time
 
-def time_test_dp(n, rep):
-    setup_stmt = """
-import random
-from tsp import tsp_dp_solve
-random.seed(0)
-d = [[random.randint(1, 10) if i != j else 0 for i in range(%(n)d + 1)]
-     for j in range(%(n)d + 1)]
-""" % {'n': n}
-    dp_timed_stmt = 'tsp_dp_solve(d)'
-    dp_time = timeit.timeit(dp_timed_stmt, setup_stmt, number=rep) / rep
-    return dp_time
-
-def get_reliable_times(n):
-    naive_time = 0
-    naive_rep = 1
+def get_reliable_time(n, func_name):
+    time = 0
+    rep = 1
     while True:
-        naive_time = time_test_naive(n, naive_rep)
-        if naive_time * naive_rep >= TARGET_TIME:
+        time = time_test(n, rep, func_name)
+        if time * rep >= TARGET_TIME:
             break
-        naive_rep *= 10
-    dp_time = 0
-    dp_rep = 1
-    while True:
-        dp_time = time_test_dp(n, dp_rep)
-        if dp_time * dp_rep >= TARGET_TIME:
-            break
-        dp_rep *= 10
-    return naive_time, dp_time
+        rep *= 10
+    return time
 
 def main():
-    print 'n\tnaive time\tdp time'
+    print 'n\tnaive time\trec time  \tdp time'
     for n in range(N_RANGE[0], N_RANGE[1] + 1):
-        naive_time, dp_time = get_reliable_times(n)
-        print '%d\t%.4e\t%.4e' % (n, naive_time, dp_time)
+        naive_time = get_reliable_time(n, 'tsp_naive_solve')
+        rec_time = get_reliable_time(n, 'tsp_rec_solve')
+        dp_time = get_reliable_time(n, 'tsp_dp_solve')
+        print '%d\t%.4e\t%.4e\t%.4e' % (n, naive_time, rec_time, dp_time)
 
 if __name__ == '__main__':
     main()
