34b1da56c47c23f35e63ecc5ef1f2e6031bab688
mchouza <mchouza@746d65ee-141b-11df-a6b0-3b3dab745ba8>
2011-02-20 21:19:04 +0000

    Commiting new version before deleting it.

--- str_table/main.c
+++ str_table/main.c
@@ -3,15 +3,7 @@
 
 int main(void)
 {
-  char str_buf[256];
-
-  str_table_init();
-
-  str_table_get(str_buf, sizeof(str_buf), str_first);
-  printf("%s\n", str_buf);
-
-  str_table_get(str_buf, sizeof(str_buf), str_second);
-  printf("%s\n", str_buf);
-
+  printf("%s\n", str_first);
+  printf("%s\n", str_second);
   return 0;
 }
