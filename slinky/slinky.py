bb772f80ea53a05c98977c867797cc011b104488
mchouza <mchouza@746d65ee-141b-11df-a6b0-3b3dab745ba8>
2011-11-03 03:17:29 +0000

    Slinky simulation - Axis labels. - Simulation parameters are now commented.

--- slinky/slinky.py
+++ slinky/slinky.py
@@ -1,11 +1,11 @@
-NT = 1000
-NX = 40
+NT = 1000 # number of timesteps
+NX = 40   # number of masses
+T = 1.0   # simulation duration
 
-L = 0.5
-K = 5.0
-M = 1.0
-G = 1.0
-T = 1.0
+L = 0.5   # slinky length
+K = 5.0   # slinky overall spring constant
+M = 1.0   # slinky mass
+G = 1.0   # gravitational acceleration
 
 def initial_x():
     sl0 = L / (NX - 1)
@@ -28,6 +28,7 @@ def get_xdot(x):
         a = sk * (x[i + 1] - x[i] - sl) / mm
         xdot[NX + i] += a
         xdot[NX + i + 1] -= a
+    #xdot[NX] = 0.0 # holding the slinky
     return xdot
 
 def rk4_step(x, dt):
@@ -54,6 +55,8 @@ def do_plot(output_fpath, hist):
         plt.plot([i * T / NT for i in range(NT+1)],
                  [x[j] for x in hist],
                  color=('black' if j == 0 or j == NX-1 else 'gray'))
+    plt.xlabel('Time')
+    plt.ylabel('Position')
     plt.savefig(output_fpath)
 
 if __name__ == '__main__':
