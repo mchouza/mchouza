fe87b472c3b6919c8aef325bfac3c4ccd24e48eb
Mariano M. Chouza <mchouza@gmail.com>
2015-11-12 23:19:31 -0300

    Adding new code using bounds for the values of 'c'.

---
+++ cubes_sum/cubes_sum.bounds.c
@@ -0,0 +1,27 @@
+#include <math.h>
+#include <stdio.h>
+
+int main(void)
+{
+    for (int a = 0; a < 1000; a++)
+    {
+        for (int b = 0; b < 1000; b++)
+        {
+        	/* 10^6 * a + 10^3 * b + c = a^3 + b^3 + c^3
+        	   as we know that 0 <= c < 10^3, we have
+        	   10^6 * a + 10^3 * b <= a^3 + b^3 + c^3 <= 10^6 * a + 10^3 * b + 999
+        	   subtracting and taking cube roots
+        	   cuberoot(10^6 * a + 10^3 * b - a^3 - b^3) <= c <= cuberoot(10^6 * a + 10^3 * b + 999 - a^3 - b^3)
+        	   by using properties of floor & ceil
+        	   ceil(cuberoot(10^6 * a + 10^3 * b - a^3 - b^3)) <= c <= floor(cuberoot(10^6 * a + 10^3 * b + 999 - a^3 - b^3)) */
+        	int c_lo = (int)fmax(ceil(pow(1000 * 1000 * a + 1000 * b - a * a * a - b * b * b, 1.0 / 3.0)), 0.0);
+        	int c_hi = (int)fmin(floor(pow(1000 * 1000 * a + 1000 * b + 999 - a * a * a - b * b * b, 1.0 / 3.0)), 999.0);
+            for (int c = c_lo; c <= c_hi; c++)
+            {
+                if (1000 * 1000 * a + 1000 * b + c == a * a * a + b * b * b + c * c * c)
+                    printf("%03d%03d%03d\n", a, b, c);
+            }
+        }
+    }
+    return 0;
+}
