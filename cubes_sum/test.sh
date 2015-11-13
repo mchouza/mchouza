aa31ef03064f24a8fcf31f3ff85809bb11dfc9f3
Mariano M. Chouza <mchouza@gmail.com>
2015-11-12 23:19:57 -0300

    Improving test script.

--- cubes_sum/test.sh
+++ cubes_sum/test.sh
@@ -1,17 +1,24 @@
 set -e
+echo -n 'Processor: '
+grep '^model name' /proc/cpuinfo | sed 's/[^:]*:\s*//' | uniq
 gcc -std=c99 -O3 cubes_sum.naive.c -o cubes_sum.naive-c
+gcc -std=c99 -O3 cubes_sum.bounds.c -lm -o cubes_sum.bounds-c
 gcc -std=c99 -O3 -msse4.1 cubes_sum.intrinsics.c -o cubes_sum.intrinsics-c
 nasm -f elf64 cubes_sum.naive-asm.s
 ld cubes_sum.naive-asm.o -o cubes_sum.naive-asm
-echo 'Naive C version'
+echo -ne '\nNaive C version'
 time ./cubes_sum.naive-c >output-naive-c
-echo 'Intrinsics C version'
+echo -ne '\nBounds-using C version'
+time ./cubes_sum.bounds-c >output-bounds-c
+echo -ne '\nSSE4.1 Intrinsics C version'
 time ./cubes_sum.intrinsics-c >output-intrinsics-c
-echo 'Naive ASM version'
+echo -ne '\nNaive ASM version'
 time ./cubes_sum.naive-asm >output-naive-asm
 sort output-naive-c -o output-naive-c
+sort output-bounds-c -o output-bounds-c
 sort output-intrinsics-c -o output-intrinsics-c
 sort output-naive-asm -o output-naive-asm
+diff output-naive-c output-bounds-c
 diff output-naive-c output-intrinsics-c
 diff output-naive-c output-naive-asm
 rm cubes_sum.*-c cubes_sum.*-asm.o cubes_sum.*-asm output-*
