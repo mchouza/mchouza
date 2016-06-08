28451594d7b24e8faba15eebf14318acbd3729af
Mariano M. Chouza <mchouza@gmail.com>
2016-06-08 20:46:54 +0100

    Adding more peak detectors and ignoring vim swap files.

--- gaussian-peak-det/gpd_test.py
+++ gaussian-peak-det/gpd_test.py
@@ -12,13 +12,58 @@ def gp_gnbg_gen(n, h, na, gslo, gshi):
     k = sigma * np.sqrt(2 * np.pi) * h
     for i in xrange(len(data)):
         data[i] += k * (phi(((i + 1) - mu) / sigma) -  phi((i - mu) / sigma))
-    return data
+    return data, mu, sigma
 
-small_peaky_gn_gen = lambda: gp_gnbg_gen(20, 3, 1.0 / 3.0, 3, 4)
+
+def small_peaky_gn_gen():
+    return gp_gnbg_gen(20, 3, 1.0 / 3.0, 3, 4)
+
+
+def big_peaky_gn_gen():
+    return gp_gnbg_gen(1000, 3, 1.0 / 3.0, 3, 4)
+
+
+def crude_max(tv):
+    return np.argmax(tv)
+
+
+def weighted_max(tv):
+    i1, i2 = tv.argsort()[-2:][::-1]
+    return (i1 * tv[i1] + i2 * tv[i2]) / (tv[i1] + tv[i2])
+
+
+def naive_centroid(tv):
+    i = np.argmax(tv)
+    m = 0
+    s = 0
+    for d in range(-5, 5):
+        m += tv[i+d] * (i + d + 0.5)
+        s += tv[i+d]
+    return m / s
+
 
 NUM_TESTS = 100
 
-TEST_GENS = (small_peaky_gn_gen,)
+TEST_GENS = (small_peaky_gn_gen, big_peaky_gn_gen)
+
+PEAK_DETECTORS = (crude_max, weighted_max, naive_centroid)
+
 
 if __name__ == '__main__':
-    print small_peaky_gn_gen()
+    np.random.seed(42)
+    for tg in TEST_GENS:
+        pd_biases = [np.zeros(NUM_TESTS) for _ in PEAK_DETECTORS]
+        for n in xrange(NUM_TESTS):
+            tv, tmu, tsigma = tg()
+            for m, pd in enumerate(PEAK_DETECTORS):
+                pd_biases[m][n] = pd(tv) - tmu
+        print 'Test %s' % tg.__name__
+        for m, pd in enumerate(PEAK_DETECTORS):
+            print '  Peak detector %s' % pd.__name__
+            print '    Average bias: %f' % np.mean(pd_biases[m])
+            print '    Average abs error: %f' % np.mean(np.abs(pd_biases[m]))
+            print '    STD of abs error: %f' % np.std(np.abs(pd_biases[m]))
+ 
+
+
+
