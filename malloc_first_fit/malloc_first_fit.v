659477007dfa54f0507e432462ac5d6e61950586
Mariano Chouza <mchouza@gmail.com>
2016-08-27 19:26:32 +0100

    Added proof that malloc increases the amount of full cells.

--- malloc_first_fit/malloc_first_fit.v
+++ malloc_first_fit/malloc_first_fit.v
@@ -33,15 +33,15 @@ Function MP_prepend_n_elems(p : MemoryPool) (n : nat) (e : MemoryPool -> MemoryP
   end.
 
 Function MP_malloc(p : MemoryPool)( n : nat) : MemoryPool * nat :=
-  match le_dec n (MP_empty_prefix_len p) with
-  | left _ _ => (MP_prepend_n_elems (MP_drop_first_n p n) n MPFull, 0)
-  | right _ _ =>
+  if le_dec n (MP_empty_prefix_len p) then
+    (MP_prepend_n_elems (MP_drop_first_n p n) n MPFull, 0)
+  else
       match p with
       | MPEnd => (MP_prepend_n_elems p n MPFull, 0)
       | MPEmpty tail => let (q, i) := MP_malloc tail n in (MPEmpty q, S i)
       | MPFull tail => let (q, i) := MP_malloc tail n in (MPFull q, S i)
       end
-  end.
+  .
 
 Function MP_count_full(p : MemoryPool) : nat :=
   match p with
@@ -86,34 +86,56 @@ Proof.
   auto.
 Qed.
 
-Lemma drop_empty_decreases :
+Lemma drop_empty_prefix :
   forall (p : MemoryPool) (n : nat),
   n <= MP_empty_prefix_len p ->
-  MP_count_full (MP_drop_first_n p n) = MP_count_full p /\
-  MP_count_empty (MP_drop_first_n p n) + n = MP_count_empty p.
+  MP_count_empty (MP_drop_first_n p n) + n = MP_count_empty p /\
+  MP_count_full (MP_drop_first_n p n) = MP_count_full p.
 Proof.
-  induction p.
-  * destruct n; simpl; omega.
-  * destruct n.
-    + simpl; omega.
-    + rewrite drop_succ_first_empty; simpl; intros.
-      assert (MP_count_empty (MP_drop_first_n p n) + n = MP_count_empty p ->
-              MP_count_empty (MP_drop_first_n p n) + S n = S (MP_count_empty p)) as Haux by omega.
-      split.
-      - apply IHp; omega.
-      - apply Haux, IHp; omega.
-  * intros; simpl in *; assert (n = 0) as n_eq_0 by omega.
-    rewrite n_eq_0; simpl; auto.
+  induction p; destruct n; simpl in *; try omega.
+  split; try apply IHp; try omega.
+  cut (MP_count_empty (MP_drop_first_n p n) + n = MP_count_empty p); try omega.
+  apply IHp; try omega.
+Qed.
+
+Lemma malloc_empty_beginning :
+  forall (p : MemoryPool) (n : nat),
+  n <= MP_empty_prefix_len p ->
+  MP_malloc p n = (MP_prepend_n_elems (MP_drop_first_n p n) n MPFull, 0).
+Proof.
+  intros; destruct p; simpl in *.
+  * case (le_dec n 0); tauto.
+  * case (le_dec n (S (MP_empty_prefix_len p))); tauto.
+  * case (le_dec n 0); tauto.
 Qed.
 
 Lemma malloc_increases_full :
   forall (p : MemoryPool) (n : nat),
   MP_count_full (fst (MP_malloc p n)) = MP_count_full p + n.
 Proof.
-  induction n, p; simpl in *; try omega.
-  * induction n; simpl in *; omega.
-  * destruct (le_dec (S n) (S (MP_empty_prefix_len p))).
-    + simpl in *.
-      replace (MP_count_full (MP_prepend_n_elems (MP_drop_first_n p n) n MPFull)) with
-              (MP_count_full (MP_drop_first_n p n) + n).
-      pose proof prepend_full_increases p n as Haux. omega.
+  induction p.
+  {
+    assert (forall n, MP_count_full (MP_prepend_n_elems MPEnd n MPFull) = n) by apply prepend_full_increases.
+    intros; simpl; destruct (le_dec n 0), n; simpl; auto.
+  } 
+  {
+    intros; destruct (le_dec n (S (MP_empty_prefix_len p))).
+    * intros; rewrite malloc_empty_beginning by (simpl; auto); simpl.
+      destruct n.
+      + simpl; omega.
+      + cut (MP_count_full (MP_prepend_n_elems (MP_drop_first_n p n) (S n) MPFull) =
+             MP_count_full (MP_drop_first_n p n) + S n).
+        - intros Hcut; rewrite Hcut.
+          cut (MP_count_full (MP_drop_first_n p n) = MP_count_full p); try omega.
+          apply drop_empty_prefix; omega.
+        - apply prepend_full_increases.
+    * intros; simpl; destruct (le_dec n (S (MP_empty_prefix_len p))); try tauto.
+      rewrite surjective_pairing with (p := MP_malloc p n); simpl.
+      apply IHp.
+  }
+  {
+    intros; simpl; destruct (le_dec n 0), n; simpl; try omega.
+    rewrite surjective_pairing with (p := MP_malloc p (S n)); simpl.
+    apply f_equal, IHp.
+  }
+Qed.
